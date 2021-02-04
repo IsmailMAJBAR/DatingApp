6447b3f3964e87a67a4de3878a50f34501740ea4
majbar <majbarismail@gmail.com>
2021-02-04 15:41:34 +0100

    Register and login functionality added to client

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { AccountService } from './../_services/account.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model)
+      .subscribe(response => {
+        console.log("🚀 ~ file: nav.component.ts ~ line 22 ~ NavComponent ~ login ~ response", response)
+      }, error => {
+        console.log("🚀 ~ file: nav.component.ts ~ line 25 ~ NavComponent ~ login ~ error", error)
+      });
+  }
+  logout() {
+    this.accountService.logout();
+
+  }
+
+}
