d1feacb0f4379d8da59a8a4bea6024af89df5b8e
majbar <majbarismail@gmail.com>
2021-02-06 00:51:27 +0100

    Adding dynamique routes, guard and bootswatch

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AccountService } from '../_services/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private toastr:ToastrService, private accountService: AccountService){}
+  canActivate(): Observable<boolean > {
+    return this.accountService.currentUsers$.pipe(
+      map(user =>{
+        if(user) return true;
+        this.toastr.error("Permission danied !")
+      })
+    );
+  }
+
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +14,8 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,14 +23,15 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model)
       .subscribe(response => {
-        console.log("🚀 ~ file: nav.component.ts ~ line 22 ~ NavComponent ~ login ~ response", response)
+        this.router.navigateByUrl('/members');
       }, error => {
-        console.log("🚀 ~ file: nav.component.ts ~ line 25 ~ NavComponent ~ login ~ error", error)
+        console.log("🚀 ~ file: nav.component.ts ~ line 25 ~ NavComponent ~ login ~ error", error);
+        this.toastr.error(error.error);
       });
   }
   logout() {
     this.accountService.logout();
-
+    this.router.navigateByUrl("/");
   }
 
 }
